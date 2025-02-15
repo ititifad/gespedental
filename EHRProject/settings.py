b8979520f314f25c3cefd9986dff5b3de72ffc2f
ititifad <dex90jay@gmail.com>
2025-02-15 14:52:01 +0300

    updates

--- EHRProject/settings.py
+++ EHRProject/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 import os
 from pathlib import Path
+from import_export.formats.base_formats import CSV, XLSX
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -28,6 +29,7 @@ DEBUG = True
 ALLOWED_HOSTS = ['*']
 
 
+IMPORT_FORMATS = [CSV, XLSX]
 # Application definition
 
 INSTALLED_APPS = [
@@ -40,7 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'patient',
     'users.apps.UsersConfig',
-
+    'import_export',
     "crispy_forms",
     "crispy_bootstrap5",
 ]
--- patient/admin.py
+++ patient/admin.py
@@ -1,17 +1,60 @@
 from django.contrib import admin
+from import_export.admin import ImportExportModelAdmin
+from .models import (
+    Patient, MedicalHistory, Treatment, ReviewofSystem,
+    Examination, Diagnosis, Investgation, Medication,
+    PaymentType, Doctor
+)
 
-from .models import *
+# Enable Import/Export for Patient
+@admin.register(Patient)
+class PatientAdmin(ImportExportModelAdmin):
+    pass
 
-admin.site.register(Patient)
-admin.site.register(MedicalHistory)
-admin.site.register(Treatment)
-admin.site.register(ReviewofSystem)
-admin.site.register(Examination)
-admin.site.register(Diagnosis)
-admin.site.register(Investgation)
-admin.site.register(Medication)
-admin.site.register(PaymentType)
-admin.site.register(Doctor)
+# Enable Import/Export for MedicalHistory
+@admin.register(MedicalHistory)
+class MedicalHistoryAdmin(ImportExportModelAdmin):
+    pass
+
+# Enable Import/Export for Treatment
+@admin.register(Treatment)
+class TreatmentAdmin(ImportExportModelAdmin):
+    pass
+
+# Enable Import/Export for ReviewofSystem
+@admin.register(ReviewofSystem)
+class ReviewofSystemAdmin(ImportExportModelAdmin):
+    pass
+
+# Enable Import/Export for Examination
+@admin.register(Examination)
+class ExaminationAdmin(ImportExportModelAdmin):
+    pass
+
+# Enable Import/Export for Diagnosis
+@admin.register(Diagnosis)
+class DiagnosisAdmin(ImportExportModelAdmin):
+    pass
+
+# Enable Import/Export for Investgation
+@admin.register(Investgation)
+class InvestgationAdmin(ImportExportModelAdmin):
+    pass
+
+# Enable Import/Export for Medication
+@admin.register(Medication)
+class MedicationAdmin(ImportExportModelAdmin):
+    pass
+
+# Enable Import/Export for PaymentType
+@admin.register(PaymentType)
+class PaymentTypeAdmin(ImportExportModelAdmin):
+    pass
+
+# Enable Import/Export for Doctor
+@admin.register(Doctor)
+class DoctorAdmin(ImportExportModelAdmin):
+    pass
 
 admin.site.site_header = "GEITA SPECIALIZED DENTAL CLINIC"
 admin.site.site_title = "GEITA SPECIALIZED DENTAL CLINIC Admin Portal"
