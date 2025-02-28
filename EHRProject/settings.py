164964332b8631d0565cda0feb2909e56f5190ee
ititifad <dex90jay@gmail.com>
2025-02-28 09:22:06 +0300

    updates

--- EHRProject/settings.py
+++ EHRProject/settings.py
@@ -87,23 +87,23 @@ WSGI_APPLICATION = 'EHRProject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'railway',
-        'USER': 'postgres',
-        'PASSWORD': 'bGWbzToLShjMuhdWfGmyJrENmawytNhy',
-        'HOST': 'autorack.proxy.rlwy.net',
-        'PORT': '59564',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'railway',
+#         'USER': 'postgres',
+#         'PASSWORD': 'bGWbzToLShjMuhdWfGmyJrENmawytNhy',
+#         'HOST': 'autorack.proxy.rlwy.net',
+#         'PORT': '59564',
+#     }
+# }
 # postgresql://postgres:bGWbzToLShjMuhdWfGmyJrENmawytNhy@autorack.proxy.rlwy.net:59564/railway
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
